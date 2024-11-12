2b01c4abd827d9fb534d29c4ee95e44d86660dcb
GBe300br <gabriela.s.silva@iesb.edu.br>
2024-11-12 02:38:09 -0300

    star wars

---
+++ __tests__/swapi.espec6.js
@@ -0,0 +1,26 @@
+const request = require('supertest')
+
+it('', async () =>{
+
+    //https://swapi.dev/api
+
+    // /planets/6
+
+    const resposta = await request('https://swapi.dev/api').get('/planets/9999');
+
+    // verificar se o status da requisisçao esta retornando falso com status 400
+
+    expect(resposta.status).toBe(404);
+    
+    //verificar o valor do corpo vazio nao encontrado
+
+    expect(resposta.body.detail).toBe('Not found'); 
+    
+    // podemos verificar tambem o corpo vazio como objeto 
+
+    expect(resposta.body).toMatchObject({
+        detail: 'Not found'
+    });
+
+    
+});
---
+++ __tests__/swapi.espec7.js
@@ -0,0 +1,29 @@
+const request = require('supertest')
+
+test('Deve visualiazar informaçoes de cadastro, quando buscar por uma pessoa existente', async () =>{
+
+    //https://swapi.dev/api
+
+    // /starships/9
+
+   const resposta = await request('https://swapi.dev/api').get('/starships/9999');
+
+  // verificar se o status da requisisçao esta retornando falso com status 400
+
+  expect(resposta.status).toBe(404);
+    
+  //verificar o valor do corpo vazio nao encontrado
+
+  expect(resposta.body.detail).toBe('Not found'); 
+  
+  // podemos verificar tambem o corpo vazio como objeto 
+
+  expect(resposta.body).toMatchObject({
+      detail: 'Not found'
+  });
+
+  
+});
+
+
+
